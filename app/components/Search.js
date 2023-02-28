04fc252e33c7b426a41eb5ad9712f328a1290d59
AmzylMohcine <amzylmohcine@gmail.com>
2023-02-28 23:34:02 +0100

    Waiting for User To Stop Typing

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,11 +1,27 @@
 import React, { useContext, useEffect } from "react"
 import DispatchContext from "../DispatchContext"
+import { useImmer } from "use-immer"
 
 function Search() {
   const appDispatch = useContext(DispatchContext)
+
+  //immer let us use lots of propreties instead of using lot uf states(useStates)
+  const [state, setState] = useImmer({
+    searchTerm: "",
+    results: [],
+    show: "neither",
+    requestCount: 0
+  })
+
   function closeSearchHandler(e) {
     e.preventDefault()
     appDispatch({ type: "closeSearch" })
+  }
+  function keyPress(e) {
+    e.preventDefault()
+    if (e.keyCode == 27) {
+      appDispatch({ type: "closeSearch" })
+    }
   }
 
   useEffect(() => {
@@ -13,11 +29,26 @@ function Search() {
     return () => document.removeEventListener("keyup", keyPress)
   }, [])
 
-  function keyPress(e) {
-    e.preventDefault()
-    if (e.keyCode == 27) {
-      appDispatch({ type: "closeSearch" })
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setState(draft => {
+        draft.requestCount++
+      })
+    }, 3000)
+    return () => clearTimeout(delay)
+  }, [state.searchTerm])
+
+  useEffect(() => {
+    if (state.requestCount) {
+      // send axios request here
     }
+  }, [state.requestCount])
+
+  function handleInput(e) {
+    const value = e.target.value
+    setState(draft => {
+      draft.searchTerm = value
+    })
   }
 
   return (
@@ -27,7 +58,7 @@ function Search() {
           <label htmlFor="live-search-field" className="search-overlay-icon">
             <i className="fas fa-search"></i>
           </label>
-          <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
+          <input onChange={handleInput} autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
           <span onClick={closeSearchHandler} className="close-live-search">
             <i className="fas fa-times-circle"></i>
           </span>
